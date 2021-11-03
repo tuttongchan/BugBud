c8a011abd7fe4f97a94711755b3a45491d3de1dd
Your Name <you@example.com>
2021-11-03 15:29:10 -0700

    second commit

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRouter from './routers/userRouter.js';
 import bugRouter from './routers/bugRouter.js';
 import imageRouter from './routers/imageRouter.js';
+import path from 'path'
 
 dotenv.config();
 
@@ -22,6 +23,13 @@ mongoose
 app.use('/api/users', userRouter);
 app.use('/api/bugs', bugRouter);
 app.use('/api/images', imageRouter)
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/client/build")));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
