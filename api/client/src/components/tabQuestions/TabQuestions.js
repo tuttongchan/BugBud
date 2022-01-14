30aa0ae8fa1035dbcd3bd4974d15ed5e8c16ea0c
Your Name <you@example.com>
2022-01-13 22:41:45 -0800

    added code editor to questions

---
+++ api/client/src/components/tabQuestions/TabQuestions.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './tabquestions.css';
+import CodeEditor from '@uiw/react-textarea-code-editor';
+
+const TabQuestions = () => {
+  return (
+    <div className="tabquestions-container">
+      <div className="tabquestions-left-container">
+        <h1>Questions</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati
+          unde vero nisi dolorum eveniet magnam, similique officia beatae quasi
+          delectus nam!
+        </p>
+      </div>
+      <div className="tabquestions-right-container">
+        <CodeEditor
+          //   value={code}
+          language="js"
+          placeholder="Please enter JS code."
+          //   onChange={(e) => setCode(e.target.value)}
+          padding={15}
+          style={{
+            width: '100%',
+            height: '70%',
+            borderRadius: '5px',
+            marginBottom: '2rem',
+            color: 'lightgreen',
+            fontSize: 16,
+            backgroundColor: '#000',
+            // backgroundColor: '#f5f5f5',
+            fontFamily:
+              'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
+          }}
+        />
+
+        <CodeEditor
+          //   value={code}
+          language="js"
+          placeholder="Please enter JS code."
+          //   onChange={(e) => setCode(e.target.value)}
+          padding={15}
+          style={{
+            width: '100%',
+            height: '25%',
+            borderRadius: '5px',
+            fontSize: 16,
+            backgroundColor: '#f5f5f5',
+            fontFamily:
+              'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default TabQuestions;
--- api/client/src/screens/QuestionsScreen/QuestionsScreen.js
+++ api/client/src/screens/QuestionsScreen/QuestionsScreen.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import './questionsScreen.css'
+import React from 'react';
+import Dashboard from '../../components/dashboard/Dashboard';
+import TabQuestions from '../../components/tabQuestions/TabQuestions';
+import Topbar from '../../components/topbar/Topbar';
+import './questionsScreen.css';
 
 const QuestionsScreen = () => {
-    return (
-        <div>
-            fffff
-        </div>
-    )
-}
+  return (
+    <div className="questionsscreen-container">
+      <Topbar />
+      <div className="bottom-container">
+        <Dashboard />
+        <TabQuestions />
+      </div>
+    </div>
+  );
+};
 
-export default QuestionsScreen
+export default QuestionsScreen;
